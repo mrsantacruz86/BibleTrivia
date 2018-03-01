982c34bf29f7bf90e80a4b569e949d43cca36f09
mrsantacruz86 <abnerdc86@gmail.com>
2018-03-01 17:07:14 -0500

    questions are now being displayed on the screen.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,34 +14,34 @@ function Question(id, qText, answer, rightAnswer,picture) {
         return false;
     }
 }
-function generateQuestions(){
-    q1 = new Question(0, "How many days did the creation last", [6,5,10,7], 0, "img0.jpg");
-    q2 = new Question(1, "In the book of Exodus God send a man to deliver the Hebrews from slavery: that man was ", ["Jesus","Noah","Moses","Abraham"], 2, "img1.jpg");
-    q3 = new Question(2, "The 10 Commandments were given by God to Moses on the Mount:", ["Carmel","Hermon","Zion","Sinai"], 3, "img2.jpg"); 
-    q4 = new Question(3, "Who did God promised that one of his descendants whould reing forever", ["Samuel","David","Darius","Herod"], 1, "img3.jpg");
+
+// var gameCountdown = setInterval(displayTime())
+
+//Shuffle the array of question,
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
 }
-// //Functions to end and reset the game
-// function startGame() {
-//     createCaracter();
-// }
-// //Calling the startGame() function
-// startGame();
+//Method to generate the questions
 
-//Array of objects containing all the characters
-// var avatarList = [yoda, luke, maul, vader];
-
-// //Functions to refresh HP
-// function refreshHP() {
-//     avatarList.forEach(function(avatar) {
-//         var $avatarCard = $(`#character-${avatar.id}`);
-//         $avatarCard.find('.avatar-hp').text(avatar.hp);
-//     })
-// }
-
-
-// Functions to make the timer works.
-var gameCountdown = setInterval(displayTime())
-
+var quiz =function (){
+    var myQuiz=[];
+    myQuiz.push(new Question(0, "How many days did the creation last", [6,5,10,7], 0, "img0.jpg"));
+    myQuiz.push(new Question(1, "In the book of Exodus God send a man to deliver the Hebrews from slavery: that man was ", ["Jesus","Noah","Moses","Abraham"], 2, "img1.jpg"));
+    myQuiz.push(new Question(2, "The 10 Commandments were given by God to Moses on the Mount:", ["Carmel","Hermon","Zion","Sinai"], 3, "img2.jpg")); 
+    myQuiz.push(new Question(3, "Who did God promised that one of his descendants whould reing forever", ["Samuel","David","Darius","Herod"], 1, "img3.jpg"));
+    shuffle(myQuiz);
+    return myQuiz;
+}
 
 //Display the question
 function displayQuestion(q) {
@@ -55,18 +55,17 @@ function displayQuestion(q) {
 }
 // Script for the DOM Manipulation 
 $(document).ready(function() {
-    generateQuestions();
-    currentQuestion = q2;
+    currentQuestion = quiz()[2];
     displayQuestion(currentQuestion);
 
-    $(".choice").click(function() {
-    //var d = this.data();
-    //    if(currentQuestion.checkAnswer($this.data('data'))){
-    //         alert('Correct!!');
-    //     }else{
-            alert(currentQuestion);
-    //     } 
-    });
+    // $(".choice").click(function() {
+    // //var d = this.data();
+    // //    if(currentQuestion.checkAnswer($this.data('data'))){
+    // //         alert('Correct!!');
+    // //     }else{
+    //         alert(currentQuestion);
+    // //     } 
+    // });
 
     // //Populate character's list
     // $.each(avatarList, function(i, element) {
