78368eab14972be6a488e55e062f2e6537d6f2d9
mrsantacruz86 <adcarballido@ymail.com>
2018-03-01 15:06:44 -0500

    adding changes to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,100 +1,75 @@
-//Avatar object declaration  
-var myAvatar;
-var defender;
-function Character(id, name, picture, hp, attackPower, counterAttackPower,) {
+//variable containing the current question 
+var currentQuestion;
+
+function Question(id, qText, answer, rightAnswer,picture) {
     this.id = id;
-    this.name = name;
+    this.qText = qText;
     this.picture = picture;
-    this.hp = hp;
-    this.attackPower = attackPower;
-    this.counterAttackPower = counterAttackPower;
-    this.attack = function(enemy){
-        this.attackPower += this.counterAttackPower;
-        enemy.hp -= this.attackPower;
-        this.hp -= enemy.counterAttackPower;
+    this.answer = answer;
+    this.rightAnswer = rightAnswer;
+    this.checkAnswer = function(answerNumber){
+        if (this.rightAnswer === answerNumber) {
+            return true;
+        }
+        return false;
     }
 }
-function createCaracter(){
-    yoda = new Character(0, "Yoda", "yoda.jpg", 120, 0, 8);
-    luke = new Character(1, "Luke Skywalker", "luke.jpg", 100, 0, 6);
-    maul = new Character(2, "Darth Maul", "maul.jpg", 150, 0, 15);
-    vader = new Character(3, "Darth Vader", "vader.jpg", 180, 0, 20);
+function generateQuestions(){
+    q1 = new Question(0, "How many days did the creation last", [6,5,10,7], 0, "img0.jpg");
+    q2 = new Question(1, "In the book of Exodus God send a man to deliver the Hebrews from slavery: that man was ", ["Jesus","Noah","Moses","Abraham"], 2, "img1.jpg");
+    q3 = new Question(2, "The 10 Commandments were given by God to Moses on the Mount:", ["Carmel","Hermon","Zion","Sinai"], 3, "img2.jpg"); 
+    q4 = new Question(3, "Who did God promised that one of his descendants whould reing forever", ["Samuel","David","Darius","Herod"], 1, "img3.jpg");
 }
-//Functions to end and reset the game
-function startGame() {
-    createCaracter();
-}
-//Calling the startGame() function
-startGame();
+// //Functions to end and reset the game
+// function startGame() {
+//     createCaracter();
+// }
+// //Calling the startGame() function
+// startGame();
 
 //Array of objects containing all the characters
-var avatarList = [yoda, luke, maul, vader];
+// var avatarList = [yoda, luke, maul, vader];
 
-//Functions to refresh HP
-function refreshHP() {
-    avatarList.forEach(function(avatar) {
-        var $avatarCard = $(`#character-${avatar.id}`);
-        $avatarCard.find('.avatar-hp').text(avatar.hp);
-    })
+// //Functions to refresh HP
+// function refreshHP() {
+//     avatarList.forEach(function(avatar) {
+//         var $avatarCard = $(`#character-${avatar.id}`);
+//         $avatarCard.find('.avatar-hp').text(avatar.hp);
+//     })
+// }
+
+
+//Display the question
+function displayQuestion(q) {
+    var questionDisplay = $('.question'); 
+    questionDisplay.data('data',q);
+    questionDisplay.html(q.qText);
+    
+    for (var i = 0; i < 4; i++) {
+        $(`#ans-${i}`).html(q.answer[i]);
+    }
 }
-
 // Script for the DOM Manipulation 
 $(document).ready(function() {
+    generateQuestions();
+    currentQuestion = q2;
+    displayQuestion(currentQuestion);
 
-    //Create avatars
-    function createAvatar(avatar, place) {
-        var card = $(`<div id="character-${avatar.id}">`); 
-        card.addClass("avatar character thumbnail");
-        card.data('data',avatar);
-        card.append("<div class = 'avatar-name'>" + avatar.name + "</div>");
-        card.append("<img src='assets/images/" + avatar.picture + "'></img>");
-        card.append("<div class = 'avatar-hp'>" + avatar.hp + "</div>");
-        place.append(card);   
-    }
-
-    //Populate character's list
-    $.each(avatarList, function(i, element) {
-        createAvatar(element, $(".characters"));
+    $(".choice").click(function() {
+        var index = thi
+       if(currentQuestion.checkAnswer($this.data('data'))){
+            alert('Correct!!');
+        }else{
+            alert('Incorrect');
+        } 
     });
+
+    // //Populate character's list
+    // $.each(avatarList, function(i, element) {
+    //     createAvatar(element, $(".characters"));
+    // });
     //OnClick event to select my character from the list of avatars
-    $('.avatar').click(function(){
-        if(!myAvatar){
-            myAvatar = $(this).data('data');
-            $(this).removeClass('character').addClass('selected-card');
-            $(this).appendTo($('.mySpace'));
-        }
-        else {
-            defender = $(this).data('data');
-            $(this).removeClass('character').addClass('enemy-card');
-            $(this).appendTo(".defender-area");
-        }
-    });
-    $('#attackBtn').click(function (){
-        var remainingDefenders = 3;
-        if (remainingDefenders > 0) {
-            myAvatar.attack(defender);
-            refreshHP();
-            if (myAvatar.hp <= 0 ) {
-                $('#attack-detail').text('You have been defeated...GAME OVER!');
-            }
-            else{
-                if (defender.hp <= 0) {
-                    $('.enemy-card').remove();
-                    defender = null;
-                    remainingDefenders --;
-                }
-                else{
-                    $('#attack-detail').html('You attacked ' + defender.name + ' for ' + myAvatar.attackPower + ' damage.</br>');
-                    $('#attack-detail').append(defender.name + ' attacked you back for ' + defender.counterAttackPower + ' damage.')   
-                }
-            }
-        }
-        else{
-            $('#attack-detail').text('You Won!...GAME OVER!');
-        }
-    });
-    $('#restartBtn').click(function () {
-        location.reload();
-    }) 
+    
 });
 
+  
