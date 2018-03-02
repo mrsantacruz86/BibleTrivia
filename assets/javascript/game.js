09fe43464bfdebf6cc612796d13af33215499309
mrsantacruz86 <adcarballido@ymail.com>
2018-03-02 03:08:33 -0500

    timer is working and choises are working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
 //variable containing the current question 
 var currentQuestion;
 
-function Question(id, qText, answer, rightAnswer,picture) {
+function Question(id, qText, choice, rightAnswer,picture) {
     this.id = id;
     this.qText = qText;
     this.picture = picture;
-    this.answer = answer;
+    this.choice = choice;
     this.rightAnswer = rightAnswer;
     this.checkAnswer = function(answerNumber){
         if (this.rightAnswer === answerNumber) {
@@ -14,8 +14,6 @@ function Question(id, qText, answer, rightAnswer,picture) {
         return false;
     }
 }
-
-// var gameCountdown = setInterval(displayTime())
 
 //Shuffle the array of question,
 /**
@@ -33,45 +31,64 @@ function shuffle(a) {
 }
 //Method to generate the questions
 
-var quiz =function (){
-    var myQuiz=[];
-    myQuiz.push(new Question(0, "How many days did the creation last", [6,5,10,7], 0, "img0.jpg"));
-    myQuiz.push(new Question(1, "In the book of Exodus God send a man to deliver the Hebrews from slavery: that man was ", ["Jesus","Noah","Moses","Abraham"], 2, "img1.jpg"));
-    myQuiz.push(new Question(2, "The 10 Commandments were given by God to Moses on the Mount:", ["Carmel","Hermon","Zion","Sinai"], 3, "img2.jpg")); 
-    myQuiz.push(new Question(3, "Who did God promised that one of his descendants whould reing forever", ["Samuel","David","Darius","Herod"], 1, "img3.jpg"));
-    shuffle(myQuiz);
-    return myQuiz;
-}
+var myQuiz=[];
+myQuiz.push(new Question(0, "How many days did the creation last", [6,5,10,7], 0, "img0.jpg"));
+myQuiz.push(new Question(1, "In the book of Exodus God send a man to deliver the Hebrews from slavery: that man was ", ["Jesus","Noah","Moses","Abraham"], 2, "img1.jpg"));
+myQuiz.push(new Question(2, "The 10 Commandments were given by God to Moses on the Mount:", ["Carmel","Hermon","Zion","Sinai"], 3, "img2.jpg")); 
+myQuiz.push(new Question(3, "Who did God promised that one of his descendants whould reing forever", ["Samuel","David","Darius","Herod"], 1, "img3.jpg"));
+shuffle(myQuiz);
+
 
 //Display the question
 function displayQuestion(q) {
-    var $questionDisplay = $('.question'); 
-    $questionDisplay.data('data',q);
+    var $questionDisplay = $('.question');
     $questionDisplay.html(q.qText);
     
     for (var i = 0; i < 4; i++) {
-        $(`#ans-${i}`).html(q.answer[i]);
+        $(`#ans-${i}`).html(q.choice[i]);
+        if(q.rightAnswer === i){
+            $(`#ans-${i}`).data('rightAnswer', true);
+        }
     }
 }
+
+
 // Script for the DOM Manipulation 
 $(document).ready(function() {
-    currentQuestion = quiz()[2];
-    displayQuestion(currentQuestion);
+    // Countdown
+    var timer = 10;
+    var gameCountdown = setInterval(function () {
+        timer--;
+        if (timer === 0) {
+            clearInterval(gameCountdown);
+            alert("perdiste el mio")
+        }
+        $('.countdown').text(timer);
+    }, 1000);
 
-    // $(".choice").click(function() {
-    // //var d = this.data();
-    // //    if(currentQuestion.checkAnswer($this.data('data'))){
-    // //         alert('Correct!!');
-    // //     }else{
-    //         alert(currentQuestion);
-    // //     } 
-    // });
+    var i = 0;
+    displayQuestion(myQuiz[i]);
 
-    // //Populate character's list
-    // $.each(avatarList, function(i, element) {
-    //     createAvatar(element, $(".characters"));
-    // });
-    //OnClick event to select my character from the list of avatars
+    $(".choice").click(function() {
+        while(i < myQuiz.length){
+            if ($(this).data('rightAnswer')){
+                alert('Correct!!');
+                i++;
+                clearInterval(gameCountdown);
+                timer = 30;
+                displayQuestion(myQuiz[i]);
+                gameCountdown();
+            }else{
+                alert('Wrong!');
+                i++;
+            }
+        }
+    });
+    $('#restartBtn').click(function(){
+        timer = 30;
+        i = 0;
+        displayQuestion(myQuiz[i]);
+    })
     
 });
 
