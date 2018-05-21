e9634a7f412f23f6e0bebbd382f8c7bd5762e0b1
Nelson Diaz <abnerdc86@gmail.com>
2018-05-21 04:20:09 -0400

    creating a countdown constructor

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,18 @@
 //variable containing the current question 
 var currentQuestion;
 
-function Question(id, qText, choice, rightAnswer,picture) {
-    this.id = id;
-    this.qText = qText;
-    this.picture = picture;
-    this.choice = choice;
-    this.rightAnswer = rightAnswer;
-    this.checkAnswer = function(answerNumber){
-        if (this.rightAnswer === answerNumber) {
-            return true;
-        }
-        return false;
+function Question(id, qText, choice, rightAnswer, picture) {
+  this.id = id;
+  this.qText = qText;
+  this.picture = picture;
+  this.choice = choice;
+  this.rightAnswer = rightAnswer;
+  this.checkAnswer = function (answerNumber) {
+    if (this.rightAnswer === answerNumber) {
+      return true;
     }
+    return false;
+  }
 }
 
 //Shuffle the array of question,
@@ -21,75 +21,117 @@ function Question(id, qText, choice, rightAnswer,picture) {
  * @param {Array} a items An array containing the items.
  */
 function shuffle(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
 }
 //Method to generate the questions
 
-var myQuiz=[];
-myQuiz.push(new Question(0, "How many days did the creation last", [6,5,10,7], 0, "img0.jpg"));
-myQuiz.push(new Question(1, "In the book of Exodus God send a man to deliver the Hebrews from slavery: that man was ", ["Jesus","Noah","Moses","Abraham"], 2, "img1.jpg"));
-myQuiz.push(new Question(2, "The 10 Commandments were given by God to Moses on the Mount:", ["Carmel","Hermon","Zion","Sinai"], 3, "img2.jpg")); 
-myQuiz.push(new Question(3, "Who did God promised that one of his descendants whould reing forever", ["Samuel","David","Darius","Herod"], 1, "img3.jpg"));
+var myQuiz = [];
+myQuiz.push(new Question(0, "How many days did the creation last", [6, 5, 10, 7], 0, "img0.jpg"));
+myQuiz.push(new Question(1, "In the book of Exodus God send a man to deliver the Hebrews from slavery: that man was ", ["Jesus", "Noah", "Moses", "Abraham"], 2, "img1.jpg"));
+myQuiz.push(new Question(2, "The 10 Commandments were given by God to Moses on the Mount:", ["Carmel", "Hermon", "Zion", "Sinai"], 3, "img2.jpg"));
+myQuiz.push(new Question(3, "Who did God promised that one of his descendants whould reing forever", ["Samuel", "David", "Darius", "Herod"], 1, "img3.jpg"));
 shuffle(myQuiz);
 
 
 //Display the question
 function displayQuestion(q) {
-    var $questionDisplay = $('.question');
-    $questionDisplay.html(q.qText);
-    
-    for (var i = 0; i < 4; i++) {
-        $(`#ans-${i}`).html(q.choice[i]);
-        if(q.rightAnswer === i){
-            $(`#ans-${i}`).data('rightAnswer', true);
-        }
+  var $questionDisplay = $('.question');
+  $questionDisplay.html(q.qText);
+
+  for (var i = 0; i < 4; i++) {
+    $(`#ans-${i}`).html(q.choice[i]);
+    if (q.rightAnswer === i) {
+      $(`#ans-${i}`).data('rightAnswer', true);
     }
+  }
+}
+
+function Countdown(initial, display){
+  this.initial = initial;
+  this.start = function(){
+    var timer = setInterval(function(){
+
+      if (initial === 0) {
+        clearInterval(timer);
+        this.display("Time is over");
+      } else{
+        initial --;
+        this.display(initial)
+      }
+    },1000);
+  },
+  this.stop = function(){
+
+  },
+  this.reset = function(){
+
+  },
+  this.display = function(text){
+    $(display).text(text);
+  }
+}
+
+function startGame(){
+  myTimer = new Countdown(20,".countdown");
+  myTimer.start();
+
+}
+
+function endGame(){
+  myTimer.start();
+  
+}
+
+function nextQuestion(){
+  myTimer.start();
+
+
 }
 
 
 // Script for the DOM Manipulation 
-$(document).ready(function() {
-    // Countdown
-    var timer = 10;
-    var gameCountdown = setInterval(function () {
-        timer--;
-        if (timer === 0) {
-            clearInterval(gameCountdown);
-            alert("perdiste el mio")
-        }
-        $('.countdown').text(timer);
-    }, 1000);
+$(document).ready(function () {
+  // Countdown
+  var timer = 10;
+  var gameCountdown = setInterval(function () {
+    timer--;
+    if (timer === 0) {
+      clearInterval(gameCountdown);
+      alert("perdiste el mio")
+    }
+    $('.countdown').text(timer);
+  }, 1000);
 
-    var i = 0;
-    displayQuestion(myQuiz[i]);
+  var i = 0;
+  displayQuestion(myQuiz[i]);
 
-    $(".choice").click(function() {
-        while(i < myQuiz.length){
-            if ($(this).data('rightAnswer')){
-                alert('Correct!!');
-                i++;
-                clearInterval(gameCountdown);
-                timer = 30;
-                displayQuestion(myQuiz[i]);
-                gameCountdown();
-            }else{
-                alert('Wrong!');
-                i++;
-            }
-        }
-    });
-    $('#restartBtn').click(function(){
+  $(".choice").click(function () {
+    while (i < myQuiz.length) {
+      if ($(this).data('rightAnswer')) {
+        alert('Correct!!');
+        i++;
+        clearInterval(gameCountdown);
         timer = 30;
-        i = 0;
         displayQuestion(myQuiz[i]);
-    })
-    
+        gameCountdown();
+      } else {
+        alert('Wrong!');
+        i++;
+      }
+    }
+  });
+  $('#restartBtn').click(function () {
+    timer = 30;
+    i = 0;
+    displayQuestion(myQuiz[i]);
+  })
+
 });
 
-  
